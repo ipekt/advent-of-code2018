8db2f9b5c23f783c0bf18233c4170316f8aa6a1f
ipek turgul <ipek.turgul@gmail.com>
2018-12-03 21:02:59 +1100

    fix day one puzzle two tests

--- day01/puzzle02/solution.exs
+++ day01/puzzle02/solution.exs
@@ -3,39 +3,54 @@ defmodule Puzzle do
   # Scan the list, passing the total
   # Filter scanned list to find the first duplicate value
   # If not found, scan the list again but use the last value from previously scanned list as accumulator
-  defstruct frequencies: ["+1", "-2"]
 
-  def find_looped_frequency(frequencies) do
+  def process(frequencies, prevAggregated, accumalated) do
+    {original, agg} = aggregate(frequencies, accumalated)
+
+    {original, prevAggregated ++ agg}
+     |> find_duplicated
+  end
+
+  def process(frequencies) do
     frequencies
-      |> Enum.map(fn frequency -> String.to_integer(frequency) end)
-      |> Enum.scan(fn f, acc -> f + acc end)
-      |> find_duplicated()
+     |> convert
+     |> aggregate(0)
+     |> find_duplicated
   end
 
-  def find_looped_frequency2(accumulator) do
-    # Map.get(%Puzzle{}, :frequencies)
-    #   |> Enum.scan(fn f, accumulator -> f + accumulator end)
-    #   |> IO.inspect
-    #   |> find_duplicated()
-  end
-
-  def find_looped_frequency() do
+  def process() do
     get_frequency_changes()
-    |> Enum.map(fn frequency -> String.to_integer(frequency) end)
-    |> Enum.scan(fn f, acc -> f + acc end)
-    |> find_duplicated()
+     |> convert
+     |> aggregate(0)
+     |> find_duplicated
   end
 
-  def find_duplicated(frequencies) do
-    duplicatedList = Enum.filter(frequencies, fn f -> Enum.count(frequencies, fn acc -> acc == f end) > 1 end)
-    IO.inspect(Enum.at(frequencies, Enum.count(frequencies) - 1))
+  def find_duplicated({originals, aggregatedOnes}) do
+    IO.inspect(aggregatedOnes)
+    duplicatedOnes = Enum.filter(aggregatedOnes, fn f -> Enum.count(aggregatedOnes, fn acc -> acc == f end) > 1 end)
 
-    if (duplicatedList == []) do
-      # get last item and call method  againa
-      find_looped_frequency2(Enum.at(frequencies, Enum.count(frequencies) - 1))
+    if (duplicatedOnes == []) do
+      process(originals, aggregatedOnes, Enum.at(aggregatedOnes, Enum.count(aggregatedOnes) - 1))
     else
-      Enum.at(duplicatedList, 0)
+      IO.inspect(Enum.at(duplicatedOnes, 0))
+      Enum.at(duplicatedOnes, 0)
     end
+  end
+
+  def aggregate(frequencies, acc), do: {frequencies, testF(frequencies, acc)}
+
+  def testF(frequencies, 0) do
+    [head | tail] = Enum.scan(frequencies, &(&1 + &2))
+    tail
+  end
+
+  def testF(frequencies, acc) do
+    Enum.scan(frequencies, acc, &(&1 + &2))
+  end
+
+  def convert(frequencies) do
+    frequencies
+    |> Enum.map(fn frequency -> String.to_integer(frequency) end)
   end
 
   def get_frequency_changes do
@@ -57,28 +72,29 @@ defmodule PuzzleTest do
   import Puzzle
 
   # test "test case 1" do
-  #   assert find_looped_frequency(["+1", "-1"]) == 0
+  #   assert process(["+1", "-1"]) == 0
   # end
 
   # test "test case 2" do
-  #   assert find_looped_frequency(["+1", "-2", "+3", "+1", "+1", "-2"]) == 2
+  #   assert process(["+1", "-2", "+3", "+1", "+1", "-2"]) == 2
   # end
 
-  test "test case 3" do
-    assert find_looped_frequency(["+3", "+3", "+4", "-2", "-4"]) == 10
-  end
-  # 3 6 10 8 4 -> 7 10 14 12 8
+  # test "test case 3" do
+  #     # 3 6 10 8 4 -> 7 10 14 12 8
+  #   assert process(["+3", "+3", "+4", "-2", "-4"]) == 10
+  # end
 
   # test "test case 4" do
-  #   assert find_looped_frequency(["-6", "+3", "+8", "+5", "-6"]) == 5
+  #   assert process(["-6", "+3", "+8", "+5", "-6"]) == 5
   # end
 
   # test "test case 5" do
-  #   assert find_looped_frequency(["+7", "+7", "-2", "-7", "-4"]) == 14
+  #   # 14 12 5 1 -> 8 15 13 6 2 -> 9 16 14 7 3
+  #   assert process(["+7", "+7", "-2", "-7", "-4"]) == 14
   # end
 
-  # test "find solution" do
-  #   assert find_looped_frequency() == 2
-  # end
+  test "find solution" do
+    assert process() == 2
+  end
 end
 
