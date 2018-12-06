2b7e2236dc4e89b48abb8d04a6eef11a5b0f2a33
ipek turgul <ipek.turgul@gmail.com>
2018-12-07 07:30:48 +1100

    improve day one puzzle one solution

--- day01/puzzle01/solution.exs
+++ day01/puzzle01/solution.exs
@@ -3,44 +3,71 @@ defmodule Puzzle do
   def find_current_frequency(frequencies) do
     frequencies
     |> Enum.map(fn frequency -> String.to_integer(frequency) end)
-    |> Enum.reduce(0, fn freq, acc -> freq + acc end)
-    |> IO.inspect()
+    |> Enum.sum()
   end
 
   def find_current_frequency() do
     get_frequency_changes()
     |> Enum.map(fn frequency -> String.to_integer(frequency) end)
-    |> Enum.reduce(0, fn freq, acc -> freq + acc end)
-    |> IO.inspect()
+    |> Enum.sum()
   end
 
   def get_frequency_changes do
     "../input.txt"
     |> Path.expand(__DIR__)
     |> File.read!()
-    |> String.trim()
-    |> String.split(~r/\n/)
+    |> String.split(~r/\n/, trim: true)
+  end
+
+  # Alternative solution from https://www.twitch.tv/videos/343661380 that is a lot faster.
+  def find_current_frequency_alt() do
+    "../input.txt"
+    |> File.stream!([], :line)
+    |> Stream.map(fn line ->
+      {integer, _leftover} = Integer.parse(line)
+       integer
+      end)
+    |> Enum.sum()
   end
 end
 
-# Tests
+# Runners
 
-ExUnit.start()
+case System.argv() do
+  ["--test"] ->
+  ExUnit.start()
 
-defmodule PuzzleTest do
-  use ExUnit.Case
+  defmodule PuzzleTest do
+    use ExUnit.Case
 
-  import Puzzle
+    import Puzzle
 
-  test "numbers are summed correctly" do
-    assert find_current_frequency(["+1", "+2", "+3"]) == 6
+    test "numbers are summed correctly" do
+      assert find_current_frequency(["+1", "+2", "+3"]) == 6
+    end
+
+    test "numbers are reduced correctly" do
+      assert find_current_frequency(["-1", "-1", "-10"]) == -12
+    end
+
+    test "numbers are calculated correctly" do
+      assert find_current_frequency(["+200", "-10", "+3"]) == 193
+    end
   end
 
-  test "numbers are reduced correctly" do
-    assert find_current_frequency(["-1", "-1", "-10"]) == -12
+  ["--alt"] ->
+  defmodule PuzzleSolutionAlt do
+    import Puzzle
+
+    find_current_frequency_alt()
+    |> IO.inspect()
   end
 
-  test "numbers are calculated correctly" do
-    assert find_current_frequency(["+200", "-10", "+3"]) == 193
+  _ ->
+  defmodule PuzzleSolution do
+    import Puzzle
+
+    find_current_frequency()
+    |> IO.inspect()
   end
 end
